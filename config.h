e7a613e25defdbb2f975f1f841643e0bac79f737
hariharan <97215411+hari06021999@users.noreply.github.com>
2023-08-01 10:32:11 +0530

    Update config.h

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 // The min amount of PWM the motors need to move. Depends on the battery, motors and controller.
 // The max amount is defined by PWMRANGE in Arduino.h
-#define PWM_MIN 60
-#define PWMRANGE 130
+#define PWM_MIN 60 // Minimum pwm
+#define PWMRANGE 130 //maximum pwm
 uint16_t frequency_right = 380; //frequency (in Hz)
 uint16_t frequency_left = 400; //frequency (in Hz)
 
@@ -10,10 +10,10 @@ void setupPins();
 void setupSerial();
 void setupWiFi();
 bool rosConnected();
-void onTwist(const geometry_msgs::Twist &msg);
-void onfront(const std_msgs::Bool &msg);
-void onfont_multi(const std_msgs::Int16 &msg);
-float mapPwm(float x, float out_min, float out_max);
+void onTwist(const geometry_msgs::Twist &msg);// joystick callback function
+void onfront(const std_msgs::Bool &msg);// front light callback function
+void onfont_multi(const std_msgs::Int16 &msg);// Multicolor light callback function
+float mapPwm(float x, float out_min, float out_max);// Mapping pwm function
 
 
 /*
@@ -26,6 +26,7 @@ A2------> Front yellow light Relay
 GPIO10-------> forward_brake Relay
 GPIO11------> reverse_brake Relay rear_light
 A3------> rear_light Relay 
+GPIO5 ------> Limit switch
 */
 const uint8_t R_PWM = 9;//D3
 const uint8_t L_PWM = 3;//D9
@@ -38,29 +39,18 @@ const uint8_t rear_light = A3;
 const uint8_t forward_brake = 10;
 const uint8_t reverse_brake = 11;
 const uint8_t limit_switch = 5;
-const uint8_t encoder_right_a=A4;
-const uint8_t encoder_right_b=A5;
-const uint8_t encoder_left_a=6;
-const uint8_t encoder_left_b=7;
-bool _connected = false;
+
+/* Left pwm and right pwm variable to access global */
 uint16_t lPwm;
 uint16_t rPwm;
+/* Industrial pc publish linear and angular value store in below variable to access through out program*/
 float linear_velocity_ref;
 float angular_velocity_ref;
 
-uint8_t d_flag=0;
-uint8_t speed_right=25,speed_left=25;
-uint8_t stflag=0;
+uint8_t d_flag=0; // direction flag
+uint8_t speed_right=25,speed_left=25; // speed increase variable
+uint8_t stflag=0; //stop flag
 
 typedef enum {ZERO,ONE,TWO,THREE,FOUR,FIVE}number;
 
-/*  PID Controller variables */
-long prevT=0;
-float eprev=0;
-float eintegral = 0;
-volatile long encoder0Pos = 0;    // encoder 1
-volatile long encoder1Pos = 0;    // encoder 2
-
-double left_kp = 1 , left_ki = 0 , left_kd = 0.0;             // modify for optimal performance
-double right_kp = 1 , right_ki = 0 , right_kd = 0.0;
 
